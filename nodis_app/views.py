a83457c6bc889447073d3573be3824db1484b0a0
ashimnepal <ashimnepal90@gmail.com>
2026-07-24 22:58:27 -0400

    Index Dash Completed, Premier Progressing

--- nodis_app/views.py
+++ nodis_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'body/Dashboard.html')
+    return render(request, 'body/Dashboard/Dashboard.html')
+
+def league(request):
+    return render(request, 'body/pages/PremierLeague.html')
